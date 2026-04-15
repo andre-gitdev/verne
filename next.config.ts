a79f39e931c5e009586703d509bdfdeb0dccafad
Andre Ferran <136394641+andre-gitdev@users.noreply.github.com>
2026-04-15 07:31:02 +0000

    feat: Sentry error monitoring configured

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import { withSentryConfig } from '@sentry/nextjs'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {}
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: 'javascript-nextjs',
+  project: 'verne',
+  silent: true,
+  widenClientFileUpload: true,
+  disableLogger: true,
+})
